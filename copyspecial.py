8d30116036a5e700d497cf122cc5d2a8281e86c3
Sarah Beverton <sebryant@gmail.com>
2020-09-07 00:59:24 -0400

    passing all tests for get_special, copy and zip

--- copyspecial.py
+++ copyspecial.py
@@ -19,39 +19,43 @@ import argparse
 
 def get_special_paths(dirname):
     """Given a dirname, returns a list of all its special files."""
+    abs_dirname = os.path.abspath(dirname)
     result = []
-    files = os.listdir(dirname)
+    files = os.listdir(abs_dirname)
     special_pattern = re.compile(r'__\w+__')
     for filename in files:
         if special_pattern.findall(filename):
-            result.append(os.path.abspath(filename))
+            result.append(os.path.join(abs_dirname, filename))
     return result
 
 
 def copy_to(path_list, dest_dir):
-    """Given a list of file paths, copies those files into the given directory"""
-    shutil.copy()
-    return
+    """Given a list of file paths, copies the files into the given directory"""
+    abs_dest_dir = os.path.abspath(dest_dir)
+    if not os.path.exists(abs_dest_dir):
+        os.makedirs(abs_dest_dir)
+    for path in path_list:
+        shutil.copy(path, abs_dest_dir)
 
 
 def zip_to(path_list, dest_zip):
-    """Given a list of file paths, zip those files up into the given zip path"""
-    # your code here
-    return
+    """Given a list of file paths, zip those files into the given zip path"""
+    abs_dest_zip = os.path.abspath(dest_zip)
+    try:
+        zip_run = 'zip -j ' + abs_dest_zip + ' ' + ' '.join(path_list)
+        subprocess.run(zip_run, shell=True)
+    except subprocess.CalledProcessError as err:
+        print(err.cmd)
+        print(err.output)
 
 
 def main(args):
     """Main driver code for copyspecial."""
-    '''need to tell it to print a list of abs paths if no args present (print whatever is
-    returned from the get_special_paths function'''
-    '''if --todir dir is present, copy the files to the given directory, creating directory if necessary (use shutil for copying)'''
-    '''if --tozip zipfile is present, zip to file and also print command saying that - use subprocess for this - if there is an error, exit and display error code and message'''
-    # This snippet will help you get started with the argparse module.
     parser = argparse.ArgumentParser()
     parser.add_argument('--todir', help='dest dir for special files')
     parser.add_argument('--tozip', help='dest zipfile for special files')
-    # TODO: add one more argument definition to parse the 'from_dir' argument
-    parser.add_argument('from_dir', help='directory to look in', nargs='+')
+    parser.add_argument('from_dir', help='directory to look in',
+                        nargs='+', type=str)
     ns = parser.parse_args(args)
 
     if not ns:
@@ -69,11 +73,14 @@ def main(args):
     # exit(1).
 
     # Your code here: Invoke (call) your functions
-    dirname = args[0]
-    
     if dir_to_use:
-        special_paths = get_special_paths(dirname)
+        special_paths = get_special_paths(''.join(dir_to_use))
         print('\n'.join(special_paths))
+        if ns.todir:
+            copy_to(special_paths, ns.todir)
+        if ns.tozip:
+            zip_to(special_paths, ns.tozip)
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
